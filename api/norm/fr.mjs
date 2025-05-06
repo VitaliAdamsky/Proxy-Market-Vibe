df8074eaf706639a96242a946b63045db0c069f4
VitaliAdamsky <vitali.adamsky@gmail.com>
2025-05-06 21:35:52 +0300

    modified return type of fr

--- api/norm/fr.mjs
+++ api/norm/fr.mjs
@@ -29,15 +29,23 @@ export default async function handler(request) {
       fetchBybitFr(bybitPerpCoins, limitKline),
     ]);
 
+    const expirationTime = calculateExpirationTime(
+      binanceFr[0]?.data.at(-1).openTime,
+      "2h"
+    );
+
     const data = normalizeFundingRateData([...binanceFr, ...bybitFr]);
 
-    return new Response(JSON.stringify(data), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "max-age=60, stale-while-revalidate=30",
-      },
-    });
+    return new Response(
+      JSON.stringify({ timeframe: "2h", expirationTime, data }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control": "max-age=60, stale-while-revalidate=30",
+        },
+      }
+    );
   } catch (error) {
     console.error("Handler error:", error);
     return new Response(
